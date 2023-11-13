ae4e1b88af2cb6c926beab8ed238826b80f7a1e9
jiechau_huang <jiechau_huang@friday.tw>
2023-11-14 03:26:15 +0800

    1st version

--- check_friDay.py
+++ check_friDay.py
@@ -4,7 +4,9 @@ from datetime import datetime, timedelta
 import re 
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
+import time
 
+#
 def check_line(line):
   if not re.fullmatch(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} OK', line):
     print("Rule 1 failed")
@@ -21,19 +23,36 @@ def check_line(line):
   return True
 
 #
-url = 'https://ailog.shopping.friday.tw/getget'
+url = 'https://ailog.shopping.friday.tw/getget1'
 
-response = requests.get(url, verify=False)
-text = response.text.splitlines()
+try_cnt = 3
+final_result = False
+i_cnt = 0
+while (final_result is False) and (i_cnt < try_cnt):
+  print(i_cnt)
+  i_cnt = i_cnt + 1
+  final_result = True
+  try:
+    response = requests.get(url, verify=False)
+    msg = response.text
+    text = response.text.splitlines()
 
-with ThreadPoolExecutor() as executor:
-  results = executor.map(check_line, text)
+    with ThreadPoolExecutor() as executor:
+      results = executor.map(check_line, text)
 
-for result in results:
-  if not result:
-    print("Check failed")
-    print(response.text)
-    exit(False)
+    for result in results:
+      if not result:
+        #print("Check failed")
+        #print(response.text)
+        final_result = False
+        #exit(False)
+  except:
+    final_result = False
+    msg = 'exception'
+  if not final_result:
+    time.sleep(5)
 
-print("All checks passed")  
+
+print(msg)
+print(final_result)  
 exit(True)
