d163c854feaa3f20186463d8d01833c9b7f1bf16
jie from ds918u <jiechau@gmail.com>
2023-12-15 01:00:37 +0800

    update cron

--- check_friDay.py
+++ check_friDay.py
@@ -6,7 +6,7 @@ import re
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 import time
-
+import subprocess
 
 # config
 def get_myconfig(_myconfig_file):
@@ -18,22 +18,21 @@ getget_url = myconfig['Check']['getget_url']
 getget_lines = myconfig['Check']['getget_lines']
 try_cnt = myconfig['Check']['try_cnt'] # 3
 try_sleep = myconfig['Check']['try_sleep'] # sec, use 60
-sms_url = myconfig['SMS']['sms_url']
-sms_username = myconfig['SMS']['sms_username']
-sms_password = myconfig['SMS']['sms_password']
-sms_defaultdst = myconfig['SMS']['sms_defaultdst']
+sms_cmd = myconfig['SMS']['sms_cmd']
 
 
 #
 def check_line(line):
+
+  # rule 1
   if not re.fullmatch(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} OK', line):
     #print("Rule 1 failed")
     return False
 
+  # rule 2
   dt = datetime.strptime(line.split(' ')[0] + ' ' + line.split(' ')[1], '%Y-%m-%d %H:%M:%S') 
   now = datetime.now()
   diff = now - dt
-
   if diff > timedelta(minutes=2):
     #print("Rule 2 failed")
     return False
@@ -61,12 +60,17 @@ while (final_result is False) and (i_cnt < try_cnt):
     with ThreadPoolExecutor() as executor:
       results = executor.map(check_line, lt_text)
 
-    for result in results:
-      if not result:
-        #print("Check failed")
-        #print(response.text)
+      for result in results:
+        #print(type(result))
+        if not result:
+          #print("Check failed")
+          #print(response.text)
+          final_result = False
+          #exit(False)
+
+    if len(lt_text) != 2:
         final_result = False
-        #exit(False)
+
   except:
     final_result = False
     msg = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' exception'
@@ -80,5 +84,18 @@ if not final_result:
   for i in range(len(lt_text)):
     if not re.fullmatch(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} OK', lt_text[i]):
       print(str(i) + ' ' + lt_text[i])
-  
+      curl_cmd = sms_cmd + ' -d text="' + str(i) + ' ' + lt_text[i] + '"'
+      rr = subprocess.run(curl_cmd, shell=True, capture_output=True, text=True)
+
+## for test
+## curl_cmd = '/usr/bin/curl -k -s -o /dev/null -X POST https://api.telegram.org/bot6666240541:AAFHp3SQNqTyKycVcCegkR5R75P5YuKgRuc/sendMessage -d chat_id=6543018944 -d text="' + str(0) + ' ' + lt_text[0] + '"'
+#curl_cmd = sms_cmd + ' -d text="' + str(0) + ' ' + lt_text[0] + '"'
+#rr = subprocess.run(curl_cmd, shell=True, capture_output=True, text=True)
+
+## for test
+#import os
+#exit_status = os.system(curl_cmd)
+#time.sleep(10)
+
+
 
--- check_friDay.sh
+++ check_friDay.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
+#cd $HOME/venv
+#source py311/bin/activate
+#cd $HOME/life_codes/alert_sms
+#while true; do
+#  python check_friDay.py
+#  sleep 60
+#done
+
+
 cd $HOME/venv
 source py311/bin/activate
 cd $HOME/life_codes/alert_sms
-while true; do
-  python check_friDay.py
-  sleep 60
-done
+python check_friDay.py >> logs/sms.txt 2>&1
+#deactivate
+current_time=$(date +"3 %Y-%m-%d %H:%M:%S")
+echo $current_time >> logs/sms.txt
