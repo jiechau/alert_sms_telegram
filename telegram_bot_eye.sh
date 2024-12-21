66b2c01e88b89eb2d66aece2af1c00427c50df20
jiechau <jiechau@gmail.com>
2024-12-22 07:33:20 +0800

    update

--- telegram_bot_eye.sh
+++ telegram_bot_eye.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ff_pid=$(ps -ef | grep "python telegram_bot_eye.py" | grep -v 'grep' | awk '{print $2}')
-kill -9 $ff_pid
+if [ ! -z "$ff_pid" ]; then
+    kill -9 $ff_pid
+fi
 source $HOME/py37t/bin/activate
 cd $HOME/life_codes/alert_sms_telegram
 python3 telegram_bot_eye.py >> /tmp/telegram_bot_eye.log 2>&1 &
