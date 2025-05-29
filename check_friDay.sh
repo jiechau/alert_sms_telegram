e164b7109e2f457af081f77b7d65bc268690d8fe
jie from ds918u <jiechau@gmail.com>
2025-05-29 15:19:15 +0800

    update

--- check_friDay.sh
+++ check_friDay.sh
@@ -9,9 +9,8 @@
 #done
 
 
-cd $HOME/venv
-source py311/bin/activate
 cd $HOME/life_codes/alert_sms_telegram
+source .venv/bin/activate
 python check_friDay.py >> logs/sms.txt 2>&1
 deactivate
 current_time=$(date +"done %Y-%m-%d %H:%M:%S")
