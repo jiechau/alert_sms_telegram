08ac6cdae3b097007ee8c727a7dd7e495d085c61
jiechau <jiechau@gmail.com>
2026-05-25 14:22:27 +0800

    update check_wednesDay.py to use OnOrderBook_datetime

--- check_wednesDay.py
+++ check_wednesDay.py
@@ -25,7 +25,7 @@ auth_tokens = myconfig['Check_wednesday'].get('auth_tokens', [])
 
 # Thresholds
 CRON_DATETIME_THRESHOLD_SEC = myconfig['Check_wednesday']['CRON_DATETIME_THRESHOLD_SEC'] # 10  # Maximum allowed time difference in seconds
-OnTrade_DATETIME_THRESHOLD_SEC = myconfig['Check_wednesday']['OnTrade_DATETIME_THRESHOLD_SEC'] # 10  # Maximum allowed time difference in seconds
+OnOrderBook_datetime_THRESHOLD_SEC = myconfig['Check_wednesday']['OnOrderBook_datetime_THRESHOLD_SEC'] # 30  # Maximum allowed time difference in seconds
 if_check_MEM = myconfig['Check_wednesday']['if_check_MEM']
 MEM_AVAIL_THRESHOLD_PERCENT = myconfig['Check_wednesday']['MEM_AVAIL_THRESHOLD_PERCENT'] # 10.0  # Minimum allowed memory availability in percent
 
@@ -182,65 +182,65 @@ _json_content =
     "stocks": {
         "i00000": {
             "symbol_c": "i00000",
-            "msg_time_c": "16:47:36",
+            "msg_time_c": "13:33:15",
             "bid1_price_c": "0",
-            "match_price_c": "34861.1600",
+            "match_price_c": "43644.4000",
             "time_value_c": 0,
             "ask1_price_c": "0",
             "match_time_c": "13:30:00",
             "strike_price": "-",
-            "OnOrderBook_datetime": "-",
-            "OnTrade_datetime": "2026-04-09 16:47:36.971331",
+            "OnOrderBook_datetime": "2026-05-25 13:33:15.944220",
+            "OnTrade_datetime": "2026-05-25 13:30:00.000000",
             "is_test_match": false
         }
     },
     "futures": {
-        "MXFE6": {
-            "symbol_c": "MXFE6",
-            "msg_time_c": "18:54:58",
-            "bid1_price_c": "34944",
-            "match_price_c": "34944",
+        "MXFG6": {
+            "symbol_c": "MXFG6",
+            "msg_time_c": "13:48:00",
+            "bid1_price_c": "43872",
+            "match_price_c": "43882",
             "time_value_c": 0,
-            "ask1_price_c": "34952",
-            "match_time_c": "18:54:16",
+            "ask1_price_c": "43887",
+            "match_time_c": "13:44:54",
             "strike_price": "-",
-            "OnOrderBook_datetime": "-",
-            "OnTrade_datetime": "2026-04-09 18:54:58.057716",
-            "basis": 82.8,
+            "OnOrderBook_datetime": "2026-05-25 13:48:00.213168",
+            "OnTrade_datetime": "2026-05-25 13:44:54.014000",
+            "basis": 237.6,
             "is_test_match": false
         }
     },
     "options": {
-        "34500": {
-            "symbol_c": "TXO34500D6",
-            "msg_time_c": "18:54:57",
-            "bid1_price_c": "685.0",
-            "match_price_c": "700.0",
-            "ask1_price_c": "705.0",
-            "match_time_c": "18:51:31",
-            "OnOrderBook_datetime_c": "-",
-            "OnTrade_datetime_c": "2026-04-09 18:54:57.569516",
-            "intrinsic_value_c": 361.1600000000035,
-            "time_value_c": 333.8399999999965,
-            "intrinsic_value_future_c": 444.0,
-            "time_value_future_c": 251.0,
+        "43200": {
+            "symbol_c": "TX443200E6",
+            "msg_time_c": "13:51:57",
+            "bid1_price_c": "640.0",
+            "match_price_c": "660.0",
+            "ask1_price_c": "720.0",
+            "match_time_c": "13:43:32",
+            "OnOrderBook_datetime_c": "2026-05-25 13:51:57.280996",
+            "OnTrade_datetime_c": "2026-05-25 13:43:32.948000",
+            "intrinsic_value_c": 444.40000000000146,
+            "time_value_c": 235.59999999999854,
+            "intrinsic_value_future_c": 682.0,
+            "time_value_future_c": -2.0,
             "is_test_match_c": false,
-            "time_value_diff_future": -128.0,
-            "strike_price": 34500,
-            "match_time_p": "18:54:13",
-            "ask1_price_p": "382.0",
-            "match_price_p": "379.0",
-            "bid1_price_p": "376.0",
-            "msg_time_p": "18:54:56",
-            "symbol_p": "TXO34500P6",
-            "OnOrderBook_datetime_p": "-",
-            "OnTrade_datetime_p": "2026-04-09 18:54:56.564626",
+            "time_value_diff_future": -165.0,
+            "strike_price": 43200,
+            "match_time_p": "13:44:59",
+            "ask1_price_p": "167.0",
+            "match_price_p": "161.0",
+            "bid1_price_p": "159.0",
+            "msg_time_p": "13:51:57",
+            "symbol_p": "TX443200Q6",
+            "OnOrderBook_datetime_p": "2026-05-25 13:51:57.280996",
+            "OnTrade_datetime_p": "2026-05-25 13:44:59.852000",
             "intrinsic_value_p": 0,
-            "time_value_p": 379.0,
+            "time_value_p": 163.0,
             "intrinsic_value_future_p": 0,
-            "time_value_future_p": 379.0,
+            "time_value_future_p": 163.0,
             "is_test_match_p": false,
-            "time_value_diff": -45.16000000000349
+            "time_value_diff": 72.59999999999854
         },
         ...
     }
@@ -248,44 +248,44 @@ _json_content =
 '''
     #print(_json_content)
     try:
-        # check stocks's elements, if now is btwn 09:00 and 13:30 and OnTrade_datetime is not empty, then check the time diff
+        # check stocks's elements, if now is btwn 09:00 and 13:30 and OnOrderBook_datetime is not empty, then check the time diff
         stocks_dict = _json_content.get('stocks', {})
         for stock_name, stock_data in stocks_dict.items():
             if isinstance(stock_data, dict):
-                on_trade_datetime_str = stock_data.get('OnTrade_datetime')
-                if on_trade_datetime_str:
-                    #print(f'{stock_name} OnTrade_datetime: {on_trade_datetime_str}')
+                on_orderbook_datetime_str = stock_data.get('OnOrderBook_datetime')
+                if on_orderbook_datetime_str:
+                    #print(f'{stock_name} OnOrderBook_datetime: {on_orderbook_datetime_str}')
                     try:
-                        on_trade_datetime = datetime.strptime(on_trade_datetime_str, "%Y-%m-%d %H:%M:%S.%f")
+                        on_orderbook_datetime = datetime.strptime(on_orderbook_datetime_str, "%Y-%m-%d %H:%M:%S.%f")
                         # Check if current time is between 09:00 and 13:30
                         now_time = datetime.now().time()
                         if now_time >= datetime.strptime("09:00:10", "%H:%M:%S").time() and now_time <= datetime.strptime("13:29:50", "%H:%M:%S").time():
-                            # Check time difference between on_trade_datetime and current time
-                            time_diff_sec = abs((datetime.now() - on_trade_datetime).total_seconds())
-                            if time_diff_sec > OnTrade_DATETIME_THRESHOLD_SEC:
-                                return {'result': False, 'msg': f'{_json_content["strategy_id"]} {stock_name} OnTrade_datetime too old: {on_trade_datetime_str}'}
+                            # Check time difference between on_orderbook_datetime and current time
+                            time_diff_sec = abs((datetime.now() - on_orderbook_datetime).total_seconds())
+                            if time_diff_sec > OnOrderBook_datetime_THRESHOLD_SEC:
+                                return {'result': False, 'msg': f'{_json_content["strategy_id"]} {stock_name} OnOrderBook_datetime too old: {on_orderbook_datetime_str}'}
                             #else:
-                            #    print(f'{stock_name} OnTrade_datetime is OK: {on_trade_datetime_str}')
+                            #    print(f'{stock_name} OnOrderBook_datetime is OK: {on_orderbook_datetime_str}')
                     except Exception as e:
                         return {'result': False, 'msg': f'{_json_content["strategy_id"]} {stock_name} datetime parse error: {str(e)}'}
 
-        # check future's elements, if OnTrade_datetime is not empty, then check the time diff
+        # check future's elements, if OnOrderBook_datetime is not empty, then check the time diff
         futures_dict = _json_content.get('futures', {})
         for future_name, future_data in futures_dict.items():
             if isinstance(future_data, dict):
-                on_trade_datetime_str = future_data.get('OnTrade_datetime', '')
+                on_orderbook_datetime_str = future_data.get('OnOrderBook_datetime', '')
                 
-                if on_trade_datetime_str:
-                    #print(f'{future_name} OnTrade_datetime: {on_trade_datetime_str}')
+                if on_orderbook_datetime_str:
+                    #print(f'{future_name} OnOrderBook_datetime: {on_orderbook_datetime_str}')
                     try:
-                        on_trade_datetime = datetime.strptime(on_trade_datetime_str, "%Y-%m-%d %H:%M:%S.%f")
+                        on_orderbook_datetime = datetime.strptime(on_orderbook_datetime_str, "%Y-%m-%d %H:%M:%S.%f")
                         
-                        # Check time difference between on_trade_datetime and current time
-                        time_diff_sec = abs((datetime.now() - on_trade_datetime).total_seconds())
-                        if time_diff_sec > OnTrade_DATETIME_THRESHOLD_SEC:
-                            return {'result': False, 'msg': f'{_json_content["strategy_id"]} {future_name} OnTrade_datetime too old: {on_trade_datetime_str}'}
+                        # Check time difference between on_orderbook_datetime and current time
+                        time_diff_sec = abs((datetime.now() - on_orderbook_datetime).total_seconds())
+                        if time_diff_sec > OnOrderBook_datetime_THRESHOLD_SEC:
+                            return {'result': False, 'msg': f'{_json_content["strategy_id"]} {future_name} OnOrderBook_datetime too old: {on_orderbook_datetime_str}'}
                         #else:
-                        #    print(f'{future_name} OnTrade_datetime is OK: {on_trade_datetime_str}')
+                        #    print(f'{future_name} OnOrderBook_datetime is OK: {on_orderbook_datetime_str}')
                     except Exception as e:
                         return {'result': False, 'msg': f'{_json_content["strategy_id"]} {future_name} datetime parse error: {str(e)}'}
 
